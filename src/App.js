b91a078df1cb855f8cbd3da6384033c2b69c2b85
marcusnygren <marcusnygren.mn@gmail.com>
2018-12-26 20:27:47 +0100

    Setup React app and read from JSON file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import myData from './vegan-main-foody.json'
+
 class App extends Component {
   render() {
+    let recept1 = myData.results[0];
+
+    console.log(recept1)
+
+    let imageUrl = recept1.image;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={"https://spoonacular.com/recipeImages/" + imageUrl} />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {myData.results[0].title}
           </p>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+
           <a
             className="App-link"
             href="https://reactjs.org"
